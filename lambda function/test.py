09e8731ebd70916a1e0030d905340ab22a9407ce
MrQuestionable <khandal.kartik05@gmail.com>
2026-08-18 19:58:47 +0530

    Refactor AWS credentials setup in test.py
    Removed redundant AWS credentials fixture and set environment variables directly in the test file.

--- lambda function/test.py
+++ lambda function/test.py
@@ -1,24 +1,20 @@
 import os
-import boto3
 import pytest
+
+os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+os.environ["AWS_SECURITY_TOKEN"] = "testing"
+os.environ["AWS_SESSION_TOKEN"] = "testing"
+os.environ["AWS_DEFAULT_REGION"] = "eu-north-1"
+
+import boto3
 from moto import mock_aws
 from PIL import Image
 import io
-
-
 from lambda_function import lambda_handler
 
 @pytest.fixture
-def aws_credentials():
-    """Mocked AWS Credentials for moto."""
-    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-    os.environ["AWS_SECURITY_TOKEN"] = "testing"
-    os.environ["AWS_SESSION_TOKEN"] = "testing"
-    os.environ["AWS_DEFAULT_REGION"] = "eu-north-1"
-
-@pytest.fixture
-def s3_and_dynamodb(aws_credentials):
+def s3_and_dynamodb():
     """Sets up a fake, temporary AWS environment."""
     with mock_aws():
         s3 = boto3.client("s3", region_name="eu-north-1")
